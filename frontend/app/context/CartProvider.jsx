b4ad278caee4b4a46ec5097a2b7a973ca37780da
Ben Fawley <benfawley0@gmail.com>
2023-08-06 19:54:36 +0100

    remove frontend directory

--- frontend/app/context/CartProvider.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { CartContextProvider } from "./cart-context";
-
-const CartProvider = ({ children }) => {
-  return <CartContextProvider>{children} </CartContextProvider>;
-};
-
-export default CartProvider;
